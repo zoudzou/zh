d46ca8bf07739576fb1101b8daedc9941e0e7318
Evan Carlson <evancarlson@8thwall.com>
2023-04-13 12:12:28 -0700

    [g8] sync default onto ceb32e14cd56c0c25e35dfbac9aead444ce22bd2

--- xrextras/src/aframe/xr-components.js
+++ xrextras/src/aframe/xr-components.js
@@ -987,19 +987,41 @@ const xrComponents = () => {
     init() {
       const hiderMaterial = new THREE.MeshStandardMaterial()
       hiderMaterial.colorWrite = false
-
       const applyHiderMaterial = (mesh) => {
-        if (!mesh) { return }
+        if (!mesh) {
+          return
+        }
         if (mesh.material) {
           mesh.material = hiderMaterial
         }
         mesh.traverse((node) => {
           if (node.isMesh) {
+            this.mat = node.material
             node.material = hiderMaterial
           }
         })
       }
-
+      applyHiderMaterial(this.el.getObject3D('mesh'))
+      this.el.addEventListener(
+        'model-loaded', () => applyHiderMaterial(this.el.getObject3D('mesh'))
+      )
+    },
+    remove() {
+      const hiderMaterial = new THREE.MeshStandardMaterial()
+      hiderMaterial.colorWrite = true
+      const applyHiderMaterial = (mesh) => {
+        if (!mesh) {
+          return
+        }
+        if (mesh.material) {
+          mesh.material = hiderMaterial
+        }
+        mesh.traverse((node) => {
+          if (node.isMesh) {
+            node.material = this.mat
+          }
+        })
+      }
       applyHiderMaterial(this.el.getObject3D('mesh'))
       this.el.addEventListener(
         'model-loaded', () => applyHiderMaterial(this.el.getObject3D('mesh'))
